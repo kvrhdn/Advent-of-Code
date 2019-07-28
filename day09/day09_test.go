3fed9b2e493015c7bd72784a53f5c6d39b71b245
Koenraad Verheyden <mail@koenraadverheyden.com>
2019-07-28 12:10:59 +0200

    Solution day 09

---
+++ day09/day09_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_processTheGarbageStream_score(t *testing.T) {
+	var cases = []struct {
+		in       string
+		expected int
+	}{
+		{"{}", 1},
+		{"{{{}}}", 6},
+		{"{{},{}}", 5},
+		{"{{{},{},{{}}}}", 16},
+		{"{<a>,<a>,<a>,<a>}", 1},
+		{"{{<ab>},{<ab>},{<ab>},{<ab>}}", 9},
+		{"{{<!!>},{<!!>},{<!!>},{<!!>}}", 9},
+		{"{{<a!>},{<a!>},{<a!>},{<ab>}}", 3},
+		{"{<{}>}", 1},
+	}
+	for _, c := range cases {
+		got, _ := processTheGarbageStream(c.in)
+		if !reflect.DeepEqual(got, c.expected) {
+			t.Errorf("processTheGarbageStream(%q) = %v, _, but expected %v, _", c.in, got, c.expected)
+		}
+	}
+}
+
+func Test_processTheGarbageStream_garbageCount(t *testing.T) {
+	var cases = []struct {
+		in       string
+		expected int
+	}{
+		{`<>`, 0},
+		{`<random characters>`, 17},
+		{`<<<<>`, 3},
+		{`<{!>}>`, 2},
+		{`<!!>`, 0},
+		{`<!!!>>`, 0},
+		{`<{o"i!a,<{i<a>`, 10},
+	}
+	for _, c := range cases {
+		_, got := processTheGarbageStream(c.in)
+		if !reflect.DeepEqual(got, c.expected) {
+			t.Errorf("processTheGarbageStream(%q) = _, %v, but expected _, %v", c.in, got, c.expected)
+		}
+	}
+}
