7ce0b3f87a56abac075df0e38f92432a2f32fcd2
Koenraad Verheyden <mail@koenraadverheyden.com>
2019-12-04 01:25:49 +0100

    move Wire logic into separate module; add console.time utils

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -1,4 +1,7 @@
-use common::grid::*;
+mod wire;
+
+use common::{console_utils::Timer, grid::*};
+use wire::*;
 
 use std::collections::HashSet;
 use std::iter::FromIterator;
@@ -10,28 +13,15 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 /// See: https://adventofcode.com/2019/day/3
 #[wasm_bindgen]
 pub fn part1(input: &str) -> Result<u32, JsValue> {
-    let wires: Vec<Wire> = parse_input(input)?
-        .into_iter()
-        .map(Wire::from_segments)
-        .collect();
+    Timer::new("rust::part1");
 
-    let position_sets: Vec<HashSet<Pos>> = wires
-        .into_iter()
-        .map(|wire| HashSet::from_iter(wire.positions.into_iter()))
-        .collect();
+    let wires = parse_input(input)?;
+    let intersections = get_intersections(wires)?;
 
-    if position_sets.len() != 2 {
-        return Err("expected input to contain exactly two wires".into());
-    }
-    let set_0 = position_sets.get(0).unwrap();
-    let set_1 = position_sets.get(1).unwrap();
-
-    let intersection: Vec<&Pos> = set_0.intersection(set_1).collect();
-
-    intersection
+    intersections
         .iter()
-        .map(|&&p| distance_origin(p))
-        .filter(|&distance| distance != 0)   // origin is also an intersection of the wires
+        .map(|&p| distance_origin(p))
+        .filter(|&distance| distance != 0) // origin is also an intersection of the wires
         .min()
         .ok_or_else(|| "expected the wires to have at least one intersection that is not the origin".into())
 }
@@ -39,105 +29,36 @@ pub fn part1(input: &str) -> Result<u32, JsValue> {
 /// See: https://adventofcode.com/2019/day/3#part2
 #[wasm_bindgen]
 pub fn part2(_input: &str) -> Result<u32, JsValue> {
+    Timer::new("rust::part2");
+
     Err("not implemented yet".into())
 }
 
-/// A wire is represented as a sequence of the positions that it covers.
-struct Wire {
-    positions: Vec<Pos>,
+fn parse_input(input: &str) -> Result<Vec<Wire>, &'static str> {
+    input
+        .lines()
+        .map(|line| -> Result<Wire, &'static str> {
+            let segments = line
+                .split(',')
+                .map(WireSegment::parse)
+                .collect::<Result<Vec<WireSegment>, &'static str>>()?;
+
+            Ok(Wire::from_segments(segments))
+        })
+        .collect()
 }
 
-impl Wire {
-    /// Construct a wire from a series of wire segments, starting at position (0, 0).
-    fn from_segments(segments: Vec<WireSegment>) -> Self {
-        let mut positions = Vec::<Pos>::new();
-        let mut curr = Pos::at(0, 0);
+fn get_intersections(wires: Vec<Wire>) -> Result<Vec<Pos>, &'static str> {
+    let position_sets = wires
+        .into_iter()
+        .map(|wire| HashSet::from_iter(wire.positions.into_iter()))
+        .collect::<Vec<HashSet<Pos>>>();
 
-        positions.push(curr);
-
-        for s in segments {
-            for _ in 0..s.length {
-                curr = curr.step(s.direction);
-
-                positions.push(curr);
-            }
-        }
-
-        Self { positions }
+    if position_sets.len() != 2 {
+        return Err("expected input to contain exactly two wires");
     }
-}
+    let set1 = &position_sets[0];
+    let set2 = &position_sets[1];
 
-/// A wire segment is a straight part of a wire, described relatively to another segment.
-#[derive(Debug, Eq, PartialEq)]
-struct WireSegment {
-    direction: Dir,
-    length: u32,
-}
-
-fn parse_input(input: &str) -> Result<Vec<Vec<WireSegment>>, &'static str> {
-    input.lines().map(parse_as_wire_segments).collect()
-}
-
-fn parse_as_wire_segments(input: &str) -> Result<Vec<WireSegment>, &'static str> {
-    input.split(',').map(parse_wire_segment).collect()
-}
-
-fn parse_wire_segment(segment: &str) -> Result<WireSegment, &'static str> {
-    let first_char = segment
-        .chars()
-        .nth(0)
-        .ok_or("could not access the first character")?;
-
-    let direction = match first_char {
-        'U' => Dir::Up,
-        'D' => Dir::Down,
-        'L' => Dir::Left,
-        'R' => Dir::Right,
-        _ => return Err("invalid direction"),
-    };
-
-    let length = segment[1..]
-        .parse::<u32>()
-        .map_err(|_| "could not parse input as integers")?;
-
-    Ok(WireSegment { direction, length })
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{Wire, WireSegment};
-    use common::grid::{Dir::*, Pos};
-
-    #[test]
-    fn parse_input_into_wire_segments() {
-        let input = "U123,D32\nL12345,R1";
-        let expected = vec![
-            vec![WireSegment { direction: Up, length: 123 }, WireSegment { direction: Down, length: 32 }],
-            vec![WireSegment { direction: Left, length: 12345 }, WireSegment { direction: Right, length: 1 }],
-        ];
-
-        let result = crate::parse_input(&input).unwrap();
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn wire_from_wire_segments() {
-        let input = vec![
-            WireSegment { direction: Up, length: 2 },
-            WireSegment { direction: Left, length: 1 },
-            WireSegment { direction: Down, length: 3 },
-            WireSegment { direction: Right, length: 1 },
-        ];
-
-        let wire = Wire::from_segments(input);
-
-        assert_eq!(wire.positions, vec![
-            Pos::at(0, 0),
-            Pos::at(0, 1), Pos::at(0, 2),
-            Pos::at(-1, 2),
-            Pos::at(-1, 1), Pos::at(-1, 0), Pos::at(-1, -1),
-            Pos::at(0, -1),
-        ]);
-    }
+    Ok(set1.intersection(&set2).cloned().collect())
 }
