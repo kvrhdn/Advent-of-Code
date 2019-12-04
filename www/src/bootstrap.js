897fdbf5ac8512fc83b77d1219883f1bc56c6046
Koenraad Verheyden <mail@koenraadverheyden.com>
2019-12-04 18:13:37 +0100

    Solution day 04, part 1 & 2

--- www/src/bootstrap.js
+++ www/src/bootstrap.js
@@ -6,3 +6,6 @@ import('./index_day02.ts')
 
 import('./index_day03.ts')
   .catch(err => console.error(`Error importing 'index_day03.ts':${err}`));
+
+  import('./index_day04.ts')
+  .catch(err => console.error(`Error importing 'index_day03.ts':${err}`));
---
+++ www/src/index_day04.ts
@@ -0,0 +1,4 @@
+import { part1, part2 } from '../../day04/pkg/day04';
+import { setupDay } from './common'
+
+setupDay('04', part1, part2);
