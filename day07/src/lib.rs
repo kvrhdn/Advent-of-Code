052ce990e1e7dcbd40893d7d3d9cfb66bf08c176
Koenraad Verheyden <mail@koenraadverheyden.com>
2019-12-08 02:05:16 +0100

    Solution day 07, part 2

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -30,30 +30,117 @@ pub fn part1(input: &str) -> Result<i32, JsValue> {
 
 /// See: https://adventofcode.com/2019/day/7#part2
 #[wasm_bindgen]
-pub fn part2(_input: &str) -> Result<u32, JsValue> {
+pub fn part2(input: &str) -> Result<i32, JsValue> {
     Timer::new("rust::part2");
 
-    Ok(0)
+    let amplifier_controller_software = load_program(input)?;
+
+    let mut largest_output = 0;
+
+    let mut phase_settings = vec![5, 6, 7, 8, 9];
+    let heap = permutohedron::Heap::new(&mut phase_settings);;
+
+    for phase_settings in heap {
+        let output = run_through_amplifiers_with_feedback(&amplifier_controller_software, &phase_settings)?;
+
+        if output > largest_output {
+            largest_output = output;
+        }
+    }
+
+    Ok(largest_output)
 }
 
 fn run_through_amplifiers(amplifier_controller_software: &[i32], phase_settings: &[i32]) -> Result<i32, &'static str> {
     let mut runtime = vec![0; amplifier_controller_software.len()];
 
-    let mut input = 0;
+    let mut signal = 0;
 
     for phase_setting in phase_settings {
         runtime.copy_from_slice(amplifier_controller_software);
         let mut amp = Computer::new(&mut runtime);
 
         amp.put_input(*phase_setting);
-        amp.put_input(input);
+        amp.put_input(signal);
         amp.run()?;
 
-        input = amp.get_output()
+        signal = amp.get_output()
             .ok_or("amplifier controller software did not output any value")?;
     }
 
-    Ok(input)
+    Ok(signal)
+}
+
+fn run_through_amplifiers_with_feedback(amplifier_controller_software: &[i32], phase_settings: &[i32]) -> Result<i32, &'static str> {
+    let mut program_amp_1 = vec![0; amplifier_controller_software.len()];
+    program_amp_1.copy_from_slice(amplifier_controller_software);
+
+    let mut amp_1 = Computer::new(&mut program_amp_1);
+    amp_1.put_input(*phase_settings.get(0).unwrap());
+
+    let mut program_amp_2 = vec![0; amplifier_controller_software.len()];
+    program_amp_2.copy_from_slice(amplifier_controller_software);
+
+    let mut amp_2 = Computer::new(&mut program_amp_2);
+    amp_2.put_input(*phase_settings.get(1).unwrap());
+
+    let mut program_amp_3 = vec![0; amplifier_controller_software.len()];
+    program_amp_3.copy_from_slice(amplifier_controller_software);
+
+    let mut amp_3 = Computer::new(&mut program_amp_3);
+    amp_3.put_input(*phase_settings.get(2).unwrap());
+
+    let mut program_amp_4 = vec![0; amplifier_controller_software.len()];
+    program_amp_4.copy_from_slice(amplifier_controller_software);
+
+    let mut amp_4 = Computer::new(&mut program_amp_4);
+    amp_4.put_input(*phase_settings.get(3).unwrap());
+
+    let mut program_amp_5 = vec![0; amplifier_controller_software.len()];
+    program_amp_5.copy_from_slice(amplifier_controller_software);
+
+    let mut amp_5 = Computer::new(&mut program_amp_5);
+    amp_5.put_input(*phase_settings.get(4).unwrap());
+
+    let mut signal = 0;
+
+    loop {
+        amp_1.put_input(signal);
+        amp_1.run()?;
+
+        signal = amp_1.get_output()
+            .ok_or("amp 1 did not have any output")?;
+
+        amp_2.put_input(signal);
+        amp_2.run()?;
+
+        signal = amp_2.get_output()
+            .ok_or("amp 2 did not have any output")?;
+
+        amp_3.put_input(signal);
+        amp_3.run()?;
+
+        signal = amp_3.get_output()
+            .ok_or("amp 3 did not have any output")?;
+
+        amp_4.put_input(signal);
+        amp_4.run()?;
+
+        signal = amp_4.get_output()
+            .ok_or("amp 4 did not have any output")?;
+
+        amp_5.put_input(signal);
+        amp_5.run()?;
+
+        signal = amp_5.get_output()
+            .ok_or("amp 5 did not have any output")?;
+
+        if amp_5.get_state() == State::HALTED {
+            break;
+        }
+    }
+
+    Ok(signal)
 }
 
 #[cfg(test)]
