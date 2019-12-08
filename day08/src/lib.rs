c31201331dd0e16b9125ab45b3e63b6fd686e6b9
Koenraad Verheyden <mail@koenraadverheyden.com>
2019-12-08 14:39:38 +0100

    Slight refactor day 08

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -17,10 +17,10 @@ pub fn part1(input: &str) -> Result<u32, JsValue> {
 
     let layer_least_0 = layers
         .iter()
-        .min_by_key(|&layer| count_char(layer, '0'))
-        .ok_or("could not find any layer")?;
+        .min_by_key(|&layer| count_chars(layer, '0'))
+        .ok_or("expected their to be at least one layer")?;
 
-    Ok(count_char(layer_least_0, '1') * count_char(layer_least_0, '2'))
+    Ok(count_chars(layer_least_0, '1') * count_chars(layer_least_0, '2'))
 }
 
 /// See: https://adventofcode.com/2019/day/8#part2
@@ -70,7 +70,7 @@ fn split_layers<'a>(input: &'a str, width: usize, height: usize) -> Vec<&'a [u8]
     input.trim_end().as_bytes().chunks(width * height).collect()
 }
 
-fn count_char(input: &[u8], c: char) -> u32 {
+fn count_chars(input: &[u8], c: char) -> u32 {
     bytecount::count(input, c as u8) as u32
 }
 
@@ -91,9 +91,9 @@ mod tests {
 
     #[test]
     fn test_count_char() {
-        assert_eq!(count_char(&[b'1', b'2', b'3', b'1', b'2', b'2'], '0'), 0);
-        assert_eq!(count_char(&[b'1', b'2', b'3', b'1', b'2', b'2'], '1'), 2);
-        assert_eq!(count_char(&[b'1', b'2', b'3', b'1', b'2', b'2'], '2'), 3);
-        assert_eq!(count_char(&[b'1', b'2', b'3', b'1', b'2', b'2'], '3'), 1);
+        assert_eq!(count_chars(&[b'1', b'2', b'3', b'1', b'2', b'2'], '0'), 0);
+        assert_eq!(count_chars(&[b'1', b'2', b'3', b'1', b'2', b'2'], '1'), 2);
+        assert_eq!(count_chars(&[b'1', b'2', b'3', b'1', b'2', b'2'], '2'), 3);
+        assert_eq!(count_chars(&[b'1', b'2', b'3', b'1', b'2', b'2'], '3'), 1);
     }
 }
