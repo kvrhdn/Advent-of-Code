31f6ef51e8ff4b7e1e6b146b4b51e48a9430688e
Koenraad Verheyden <mail@koenraadverheyden.com>
2019-12-15 23:58:06 +0100

    Refactor day 08

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -13,7 +13,7 @@ const HEIGHT: usize = 6;
 pub fn part1(input: &str) -> Result<u32, JsValue> {
     Timer::new("rust::part1");
 
-    let layers = split_layers(input, WIDTH, HEIGHT);
+    let layers = as_layers(input, WIDTH * HEIGHT);
 
     let layer_least_0 = layers
         .iter()
@@ -30,33 +30,13 @@ pub fn part1(input: &str) -> Result<u32, JsValue> {
 pub fn part2(input: &str) -> Result<JsValue, JsValue> {
     Timer::new("rust::part2");
 
-    let layers = split_layers(input, WIDTH, HEIGHT);
-
-    let mut image = [' '; WIDTH * HEIGHT];
-
-    for x in 0..WIDTH {
-        for y in 0..HEIGHT {
-            let pixel_index = (y * WIDTH) + x;
-
-            let pixel: u8 = layers
-                .iter()
-                .map(|layer| layer[pixel_index])
-                .filter(|&pixel| pixel != b'2') // 2 is transparent
-                .nth(0)
-                .ok_or("did find not any non-transparant pixel")?;
-
-            image[pixel_index] = match pixel {
-                b'0' => ' ',
-                b'1' => '█',
-                _ => return Err("expected all pixels to be '0' or '1'".into()),
-            };
-        }
-    }
+    let layers = as_layers(input, WIDTH * HEIGHT);
+    let image = flatten_layers(layers, WIDTH * HEIGHT)?;
 
     let string = image
         .chunks_exact(WIDTH)
-        .map(|chunk| chunk.iter().cloned().collect::<String>())
-        .collect::<Vec<String>>()
+        .map(collect_to_string)
+        .collect::<Vec<_>>()
         .join("\n");
 
     console::log_1(&"The solution to day 08, part 2:".into());
@@ -65,12 +45,37 @@ pub fn part2(input: &str) -> Result<JsValue, JsValue> {
     Ok("See the console".into())
 }
 
-fn split_layers<'a>(input: &'a str, width: usize, height: usize) -> Vec<&'a [u8]> {
+fn as_layers<'a>(input: &'a str, layer_size: usize) -> Vec<&'a [u8]> {
     input
         .trim_end()
         .as_bytes()
-        .chunks_exact(width * height)
+        .chunks_exact(layer_size)
         .collect()
+}
+
+fn flatten_layers(layers: Vec<&[u8]>, layer_size: usize) -> Result<Vec<char>, &'static str> {
+    let mut image = vec![' '; layer_size];
+
+    for i in 0..layer_size {
+        let pixel: u8 = layers
+            .iter()
+            .map(|layer| layer[i])
+            .filter(|&pixel| pixel != b'2') // 2 is transparent
+            .nth(0)
+            .ok_or("did find not any non-transparant pixel")?;
+
+        image[i] = match pixel {
+            b'0' => ' ',
+            b'1' => '█',
+            _ => return Err("expected all pixels to be '0' or '1'".into()),
+        };
+    }
+
+    Ok(image)
+}
+
+fn collect_to_string(chars: &[char]) -> String {
+    chars.iter().copied().collect()
 }
 
 #[cfg(test)]
@@ -78,9 +83,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_split_layers() {
+    fn test_as_layers() {
         assert_eq!(
-            split_layers("123456789012", 3, 2),
+            as_layers("123456789012", 6),
             vec![
                 &[b'1', b'2', b'3', b'4', b'5', b'6'],
                 &[b'7', b'8', b'9', b'0', b'1', b'2'],
