1dca7db8867fb4b1bd499275553d17535d4f9839
Koenraad Verheyden <github@kvrhdn.dev>
2020-11-26 01:10:49 +0100

    AoC 2019: refactor day 1 and 2

--- advent-of-code-2019/src/day01.rs
+++ advent-of-code-2019/src/day01.rs
@@ -1,48 +1,24 @@
 use aoc_runner_derive::aoc;
 
-/// Calculate the sum of the fuel requirements for all of the modules on the spacecraft.
-/// See: https://adventofcode.com/2019/day/1
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &str) -> Result<i32, &'static str> {
-    let sum = parse_input(input)?
-        .iter()
-        .map(|&mass| fuel_required(mass))
-        .sum();
-
-    Ok(sum)
+fn solve_part1(input: &str) -> i32 {
+    parse_input(input).map(fuel_required).sum()
 }
 
-/// Calculate the sum of the fuel requirements for all of the modules on the spacecraft, also
-/// taking into account the mass of the added fuel.
-/// See: https://adventofcode.com/2019/day/1#part2
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &str) -> Result<i32, &'static str> {
-    let sum = parse_input(input)?
-        .iter()
-        .map(|&mass| total_fuel_required(mass))
-        .sum();
-
-    Ok(sum)
+fn solve_part2(input: &str) -> i32 {
+    parse_input(input).map(total_fuel_required).sum()
 }
 
-/// Parse the input (a list of modules their masses) as a list of integers.
-fn parse_input(input: &str) -> Result<Vec<i32>, &'static str> {
-    input
-        .lines()
-        .map(|l| {
-            l.parse::<i32>()
-                .map_err(|_| "could not parse input as integers")
-        })
-        .collect()
+fn parse_input(input: &str) -> impl Iterator<Item = i32> + '_ {
+    input.lines().map(|l| l.parse::<i32>().unwrap())
 }
 
-/// Fuel required to carry the given mass.
 fn fuel_required(mass: i32) -> i32 {
     // integer dision already rounds down
     (mass / 3) - 2
 }
 
-/// Total fuel required to carry the given mass, including the mass of the fuel itself.
 fn total_fuel_required(mass: i32) -> i32 {
     let fuel = fuel_required(mass);
     if fuel <= 0 {
@@ -57,31 +33,37 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_input_test() {
-        assert_eq!(parse_input("123\n-5\n0\n"), Ok(vec![123, -5, 0]));
-        assert!(parse_input("123\na\n0\n").is_err());
+    fn examples_fuel_required() {
+        #[rustfmt::skip]
+        let examples = vec![
+            (12, 2),
+            (14, 2),
+            (1969, 654),
+            (100756, 33583),
+        ];
+        for (input, expected) in examples {
+            assert_eq!(fuel_required(input), expected);
+        }
     }
 
     #[test]
-    fn fuel_required_test() {
-        assert_eq!(fuel_required(12), 2);
-        assert_eq!(fuel_required(14), 2);
-        assert_eq!(fuel_required(1969), 654);
-        assert_eq!(fuel_required(100_756), 33_583);
-    }
-
-    #[test]
-    fn total_fuel_required_test() {
-        assert_eq!(total_fuel_required(14), 2);
-        assert_eq!(total_fuel_required(1969), 966);
-        assert_eq!(total_fuel_required(100_756), 50_346);
+    fn examples_total_fuel_required() {
+        #[rustfmt::skip]
+        let examples = vec![
+            (14, 2),
+            (1969, 966),
+            (100756, 50346),
+        ];
+        for (input, expected) in examples {
+            assert_eq!(total_fuel_required(input), expected);
+        }
     }
 
     #[test]
     fn real_input() {
         let input = include_str!("../input/2019/day1.txt");
 
-        assert_eq!(solve_part1(input), Ok(3266516));
-        assert_eq!(solve_part2(input), Ok(4896902));
+        assert_eq!(solve_part1(input), 3266516);
+        assert_eq!(solve_part2(input), 4896902);
     }
 }
--- advent-of-code-2019/src/day02.rs
+++ advent-of-code-2019/src/day02.rs
@@ -1,36 +1,34 @@
+use crate::intcode::{load_program, Computer};
 use aoc_runner_derive::aoc;
-use crate::intcode::*;
 
-/// See: https://adventofcode.com/2019/day/2
 #[aoc(day2, part1)]
-pub fn solve_part1(input: &str) -> Result<i64, &'static str> {
-    let mut computer = Computer::new_from_input(input)?;
+fn solve_part1(input: &str) -> i64 {
+    let mut computer = Computer::new_from_input(input).unwrap();
     set_noun_and_verb(&mut computer, 12, 2);
 
-    computer.run()?;
+    computer.run().unwrap();
 
-    Ok(computer.get(0))
+    computer.get(0)
 }
 
-/// See: https://adventofcode.com/2019/day/2#part2
 #[aoc(day2, part2)]
-pub fn solve_part2(input: &str) -> Result<i64, &'static str> {
-    let program = load_program(input)?;
+fn solve_part2(input: &str) -> i64 {
+    let program = load_program(input).unwrap();
 
     for noun in 0..=99 {
         for verb in 0..=99 {
             let mut computer = Computer::new(program.clone());
             set_noun_and_verb(&mut computer, noun, verb);
 
-            computer.run()?;
+            computer.run().unwrap();
 
             if computer.get(0) == 19_690_720 {
-                return Ok((100 * noun) + verb);
+                return (100 * noun) + verb;
             }
         }
     }
 
-    Err("could not find a noun and verb".into())
+    panic!("could not find a noun and verb")
 }
 
 fn set_noun_and_verb(computer: &mut Computer, noun: i64, verb: i64) {
@@ -42,12 +40,11 @@ fn set_noun_and_verb(computer: &mut Computer, noun: i64, verb: i64) {
 mod tests {
     use super::*;
 
-
     #[test]
     fn real_input() {
         let input = include_str!("../input/2019/day2.txt");
 
-        assert_eq!(solve_part1(input), Ok(4484226));
-        assert_eq!(solve_part2(input), Ok(5696));
+        assert_eq!(solve_part1(input), 4484226);
+        assert_eq!(solve_part2(input), 5696);
     }
 }
