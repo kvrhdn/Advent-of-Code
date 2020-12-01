46a2365ea4803fa433b245de29c26c962b416159
Koenraad Verheyden <github@kvrhdn.dev>
2020-12-01 08:55:03 +0100

    AoC 2020: day 1 - use tuple_combinations

--- advent-of-code-2020/src/day01.rs
+++ advent-of-code-2020/src/day01.rs
@@ -6,25 +6,38 @@ fn parse_input(input: &str) -> Vec<u32> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-#[aoc(day1, part1)]
-fn solve_part1(input: &[u32]) -> u32 {
-    product_of_entries_that_make_2020(input, 2)
+#[aoc(day1, part1, vec_combinations)]
+fn solve_part1_combinations(input: &[u32]) -> u32 {
+    input
+        .iter()
+        .copied()
+        .combinations(2)
+        .filter(|values| values.iter().sum::<u32>() == 2020)
+        .map(|values| values.iter().product())
+        .next()
+        .unwrap()
+}
+
+#[aoc(day1, part1, tuple_combinations)]
+fn solve_part1_tuple_combinations(input: &[u32]) -> u32 {
+    input
+        .iter()
+        .tuple_combinations::<(&u32, &u32)>()
+        .filter(|&(v1, v2)| v1 + v2 == 2020)
+        .map(|(v1, v2)| v1 * v2)
+        .next()
+        .unwrap()
 }
 
 #[aoc(day1, part2)]
 fn solve_part2(input: &[u32]) -> u32 {
-    product_of_entries_that_make_2020(input, 3)
-}
-
-fn product_of_entries_that_make_2020(entries: &[u32], window_size: usize) -> u32 {
-    entries
+    input
         .iter()
-        .copied()
-        .combinations(window_size)
-        .find(|values| values.iter().sum::<u32>() == 2020)
+        .tuple_combinations::<(&u32, &u32, &u32)>()
+        .filter(|(&v1, &v2, &v3)| v1 + v2 + v3 == 2020)
+        .map(|(v1, v2, v3)| v1 * v2 * v3)
+        .next()
         .unwrap()
-        .iter()
-        .product()
 }
 
 #[cfg(test)]
@@ -41,7 +54,8 @@ mod tests {
 1456"#;
         let input = parse_input(input);
 
-        assert_eq!(solve_part1(&input), 514579);
+        assert_eq!(solve_part1_combinations(&input), 514579);
+        assert_eq!(solve_part1_tuple_combinations(&input), 514579);
         assert_eq!(solve_part2(&input), 241861950);
     }
 
@@ -50,7 +64,8 @@ mod tests {
         let input = include_str!("../input/2020/day1.txt");
         let input = parse_input(input);
 
-        assert_eq!(solve_part1(&input), 381699);
+        assert_eq!(solve_part1_combinations(&input), 381699);
+        assert_eq!(solve_part1_tuple_combinations(&input), 381699);
         assert_eq!(solve_part2(&input), 111605670);
     }
 }
