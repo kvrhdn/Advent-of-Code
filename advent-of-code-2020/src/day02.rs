134f585c7f02306f1094a303019fe570009e9876
Koenraad Verheyden <github@kvrhdn.dev>
2020-12-02 08:40:56 +0100

    AoC 2020: day 2

---
+++ advent-of-code-2020/src/day02.rs
@@ -0,0 +1,80 @@
+use aoc_runner_derive::aoc;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+struct Password<'a> {
+    min: usize,
+    max: usize,
+    c: char,
+    password: &'a str,
+}
+
+impl<'a> Password<'a> {
+    fn parse(password: &'a str) -> Self {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
+        }
+
+        let captures = RE.captures(password).unwrap();
+
+        Self {
+            min: captures.get(1).unwrap().as_str().parse().unwrap(),
+            max: captures.get(2).unwrap().as_str().parse().unwrap(),
+            c: captures.get(3).unwrap().as_str().chars().next().unwrap(),
+            password: captures.get(4).unwrap().as_str(),
+        }
+    }
+
+    fn is_valid_v1(&self) -> bool {
+        let count = self.password.chars().filter(|&c| c == self.c).count();
+
+        count >= self.min && count <= self.max
+    }
+
+    fn is_valid_v2(&self) -> bool {
+        self.password
+            .char_indices()
+            // convert to 1-based indexing
+            .filter(|&(i, _)| i + 1 == self.min || i + 1 == self.max)
+            .filter(|&(_, c)| c == self.c)
+            .count()
+            == 1
+    }
+}
+
+fn parse_passwords(input: &str) -> impl Iterator<Item = Password> {
+    input.lines().map(|line| Password::parse(line))
+}
+
+#[aoc(day2, part1)]
+fn solve_part1(input: &str) -> usize {
+    parse_passwords(input).filter(|p| p.is_valid_v1()).count()
+}
+
+#[aoc(day2, part2)]
+fn solve_part2(input: &str) -> usize {
+    parse_passwords(input).filter(|p| p.is_valid_v2()).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = r#"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc"#;
+
+        assert_eq!(solve_part1(input), 2);
+        assert_eq!(solve_part2(input), 1);
+    }
+
+    #[test]
+    fn real_input() {
+        let input = include_str!("../input/2020/day2.txt");
+
+        assert_eq!(solve_part1(input), 636);
+        assert_eq!(solve_part2(input), 588);
+    }
+}
--- advent-of-code-2020/src/lib.rs
+++ advent-of-code-2020/src/lib.rs
@@ -1,5 +1,6 @@
 use aoc_runner_derive::aoc_lib;
 
 mod day01;
+mod day02;
 
 aoc_lib! { year = 2020 }
