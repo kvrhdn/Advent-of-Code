89b7ebb33b421d6baa62434528b88697c7031ddd
Koenraad Verheyden <github@kvrhdn.dev>
2020-12-06 02:54:37 +0100

    AoC 2020: day 5 - optimize

--- advent-of-code-2020/src/day05.rs
+++ advent-of-code-2020/src/day05.rs
@@ -1,8 +1,12 @@
 use aoc_runner_derive::aoc;
-use itertools::Itertools;
+use itertools::{
+    Itertools,
+    MinMaxResult::{MinMax, NoElements, OneElement},
+};
 
 // (row, col)
-type Seat = (u32, u32);
+type Seat = (u16, u16);
+type SeatId = u16;
 
 fn parse_seat(input: &str) -> Seat {
     let (row, col) = input.split_at(7);
@@ -10,65 +14,47 @@ fn parse_seat(input: &str) -> Seat {
     (parse_binary(row, 'B'), parse_binary(col, 'R'))
 }
 
-fn parse_binary(input: &str, high: char) -> u32 {
-    let mut weight = 2u32.pow(input.len() as u32 - 1);
+fn parse_binary(input: &str, high: char) -> u16 {
     let mut value = 0;
 
     for c in input.chars() {
+        value *= 2;
+
         if c == high {
-            value += weight;
+            value += 1;
         }
-        weight /= 2;
     }
 
     value
 }
 
-fn seat_id(seat: Seat) -> u32 {
-    (seat.0 * 8) + seat.1
+fn seat_id(seat: Seat) -> SeatId {
+    seat.0 * 8 + seat.1
 }
 
-fn process_input(input: &str) -> impl Iterator<Item = Seat> + '_ {
-    input.lines().map(parse_seat)
+fn process_input(input: &str) -> impl Iterator<Item = SeatId> + '_ {
+    input.lines().map(parse_seat).map(seat_id)
 }
 
 #[aoc(day5, part1)]
-fn solve_part1(input: &str) -> u32 {
-    process_input(input).map(seat_id).max().unwrap()
+fn solve_part1(input: &str) -> SeatId {
+    process_input(input).max().unwrap()
 }
 
 #[aoc(day5, part2)]
 fn solve_part2(input: &str) -> u32 {
-    let mut seats = process_input(input).collect::<Vec<_>>();
+    let seats = process_input(input).collect::<Vec<_>>();
 
-    // sort by row
-    seats.sort_unstable_by_key(|seat| seat.0);
+    // we assume the seat IDs are consecutive from min to max (except for the one missing seat)
+    let (min, max) = match seats.iter().minmax() {
+        MinMax(min, max) => (*min as u32, *max as u32),
+        OneElement(_) | NoElements => unreachable!(),
+    };
+    let expected_sum = ((max - min + 1) * (min + max)) / 2;
 
-    let min_row = seats[0].0;
-    let max_row = seats[seats.len() - 1].0;
+    let actual_sum = seats.iter().map(|&s| s as u32).sum::<u32>();
 
-    for (row, row_seats) in &seats.into_iter().group_by(|seat| seat.0) {
-        if row == min_row || row == max_row {
-            continue;
-        }
-
-        let mut row_seats = row_seats.map(|seat| seat.1).collect::<Vec<_>>();
-
-        if row_seats.len() != 8 {
-            row_seats.sort_unstable();
-
-            let missing_seat_id = row_seats
-                .into_iter()
-                .enumerate()
-                .find(|&(i, col)| i as u32 != col)
-                .map(|(i, _)| seat_id((row, i as u32)))
-                .unwrap();
-
-            return missing_seat_id;
-        }
-    }
-
-    panic!("could not find a missing seat");
+    expected_sum - actual_sum
 }
 
 #[cfg(test)]
@@ -77,7 +63,7 @@ mod tests {
 
     #[test]
     fn examples_part1() {
-        let examples: Vec<(&str, Seat, u32)> = vec![
+        let examples = vec![
             ("BFFFBBFRRR", (70, 7), 567),
             ("FFFBBBFRRR", (14, 7), 119),
             ("BBFFBBFRLL", (102, 4), 820),
