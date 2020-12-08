4f8ac53d1795f1cd01185b09d767345f307b9a54
Koenraad Verheyden <github@kvrhdn.dev>
2020-12-08 12:35:21 +0100

    AoC 2020: day 8 - refactor

--- advent-of-code-2020/src/day08.rs
+++ advent-of-code-2020/src/day08.rs
@@ -1,21 +1,37 @@
-use aoc_runner_derive::aoc;
+use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 use std::collections::HashSet;
+use ExecutionResult::{InfiniteLoopDetected, Terminated};
 
-fn parse_program(input: &str) -> Vec<(&str, i32)> {
-    input
-        .lines()
-        .map(|line| {
-            let (ir, value) = line.split(' ').collect_tuple().unwrap();
-
-            (ir, value.parse().unwrap())
-        })
-        .collect()
+#[derive(Copy, Clone)]
+enum Instruction {
+    Acc(i32),
+    Jmp(i32),
+    Nop(i32),
 }
 
-// Execute the program and return the value of the accumulator and whether the
-// program executed cleanly (i.e. not stuck in a loop).
-fn execute(program: &[(&str, i32)]) -> (i32, bool) {
+impl Instruction {
+    fn parse(s: &str) -> Self {
+        let (ir, value) = s.splitn(2, ' ').collect_tuple().unwrap();
+
+        let value = value.parse().unwrap();
+
+        match ir {
+            "acc" => Instruction::Acc(value),
+            "jmp" => Instruction::Jmp(value),
+            "nop" => Instruction::Nop(value),
+            _ => panic!("unsupported instruction: {}", ir),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum ExecutionResult {
+    InfiniteLoopDetected,
+    Terminated,
+}
+
+fn execute(program: &[Instruction]) -> (i32, ExecutionResult) {
     let mut acc = 0;
     let mut ptr = 0;
 
@@ -24,67 +40,57 @@ fn execute(program: &[(&str, i32)]) -> (i32, bool) {
     loop {
         // loop detector
         if !visited.insert(ptr) {
-            return (acc, false);
+            return (acc, InfiniteLoopDetected);
         }
 
         if ptr < 0 || ptr >= program.len() as i32 {
-            return (acc, true);
+            return (acc, Terminated);
         }
 
-        let (ir, value) = program[ptr as usize];
-
-        match ir {
-            "nop" => {}
-            "acc" => acc += value,
-            "jmp" => ptr += value - 1,
-            _ => panic!("unexpected instruction {}", ir),
+        match program[ptr as usize] {
+            Instruction::Acc(value) => acc += value,
+            Instruction::Jmp(value) => ptr += value - 1,
+            Instruction::Nop(_) => {}
         }
 
         ptr += 1;
     }
 }
 
-#[aoc(day8, part1)]
-fn solve_part1(input: &str) -> i32 {
-    let program = parse_program(input);
+#[aoc_generator(day8)]
+fn parse_program(input: &str) -> Vec<Instruction> {
+    input.lines().map(Instruction::parse).collect()
+}
 
+#[aoc(day8, part1)]
+fn solve_part1(program: &[Instruction]) -> i32 {
     execute(&program).0
 }
 
 #[aoc(day8, part2)]
-fn solve_part2(input: &str) -> i32 {
-    let program = parse_program(input);
-
-    let manipulated_programs = (0..program.len())
+fn solve_part2(program: &[Instruction]) -> i32 {
+    (0..program.len())
         .filter_map(|index| {
-            let (ir, _) = program[index];
-
-            if ir != "nop" && ir != "jmp" {
+            if !matches!(program[index], Instruction::Nop(_) | Instruction::Jmp(_)) {
                 return None;
             }
 
-            let mut manipulated_program = program.clone();
+            let mut manipulated_program = program.to_vec();
 
-            let (ir, _) = manipulated_program.get_mut(index).unwrap();
+            let ir = manipulated_program.get_mut(index).unwrap();
             *ir = match *ir {
-                "nop" => "jmp",
-                "jmp" => "nop",
-                _ => unreachable!(),
+                Instruction::Jmp(value) => Instruction::Nop(value),
+                Instruction::Nop(value) => Instruction::Jmp(value),
+                Instruction::Acc(_) => unreachable!(),
             };
 
             Some(manipulated_program)
         })
-        .collect::<Vec<_>>();
-
-    for manipulated_program in manipulated_programs {
-        let (acc, clean_exit) = execute(&manipulated_program);
-
-        if clean_exit {
-            return acc;
-        }
-    }
-
-    panic!("No manipulated program executed cleanly!");
+        .find_map(|program| match execute(&program) {
+            (acc, Terminated) => Some(acc),
+            (_, InfiniteLoopDetected) => None,
+        })
+        .expect("No manipulated program terminated.")
 }
 
 #[cfg(test)]
@@ -103,16 +109,18 @@ acc -99
 acc +1
 jmp -4
 acc +6";
+        let program = parse_program(input);
 
-        assert_eq!(solve_part1(input), 5);
-        assert_eq!(solve_part2(input), 8);
+        assert_eq!(solve_part1(&program), 5);
+        assert_eq!(solve_part2(&program), 8);
     }
 
     #[test]
     fn real_input() {
         let input = include_str!("../input/2020/day8.txt");
+        let program = parse_program(input);
 
-        assert_eq!(solve_part1(input), 1675);
-        assert_eq!(solve_part2(input), 1532);
+        assert_eq!(solve_part1(&program), 1675);
+        assert_eq!(solve_part2(&program), 1532);
     }
 }
