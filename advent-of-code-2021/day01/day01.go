e121f0a35151145694231a7bfadff52cf235face
Koenraad Verheyden <koenraad.verheyden@grafana.com>
2021-12-01 12:07:31 +0100

    AoC 2021: day 01

--- advent-of-code-2021/day01/day01.go
+++ advent-of-code-2021/day01/day01.go
@@ -2,17 +2,57 @@ package day01
 
 import (
 	"context"
-	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/kvrhdn/advent-of-code/advent-of-code-2021/aoc"
 )
 
-var Solution = aoc.NewDayGen(generator, part1, nil)
+var Solution = aoc.NewDayGen(generator, part1, part2)
 
-func generator(ctx context.Context, input string) (int, error) {
-	return len(input), nil
+func generator(ctx context.Context, input string) (ints []int, err error) {
+	for _, line := range strings.Split(input, "\n") {
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+		ints = append(ints, i)
+	}
+	return
 }
 
-func part1(ctx context.Context, input int) (string, error) {
-	return fmt.Sprint(input), nil
+func part1(ctx context.Context, input []int) (string, error) {
+	increases := 0
+
+	prevDepth := input[0]
+	for _, depth := range input[1:] {
+		if depth > prevDepth {
+			increases += 1
+		}
+		prevDepth = depth
+	}
+
+	return strconv.Itoa(increases), nil
+}
+
+func part2(ctx context.Context, input []int) (string, error) {
+	increases := 0
+
+	prevDepth := sum(input[0:3])
+	for i := range input[1 : len(input)-1] {
+		depth := sum(input[i : i+3])
+		if depth > prevDepth {
+			increases += 1
+		}
+		prevDepth = depth
+	}
+
+	return strconv.Itoa(increases), nil
+}
+
+func sum(slice []int) (sum int) {
+	for _, s := range slice {
+		sum += s
+	}
+	return
 }
--- advent-of-code-2021/day01/day01_test.go
+++ advent-of-code-2021/day01/day01_test.go
@@ -1,9 +1,37 @@
 package day01
 
 import (
+	"context"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSolution(t *testing.T) {
-	Solution.VerifySolution(t, 1, "0", "")
+	Solution.VerifySolution(t, 1, "1184", "1158")
+}
+
+func TestExample(t *testing.T) {
+	ctx := context.Background()
+	example := `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`
+
+	gen, err := generator(ctx, example)
+	assert.NoError(t, err)
+
+	output1, err := part1(ctx, gen)
+	assert.NoError(t, err)
+	assert.Equal(t, "7", output1)
+
+	output2, err := part2(ctx, gen)
+	assert.NoError(t, err)
+	assert.Equal(t, "5", output2)
 }
